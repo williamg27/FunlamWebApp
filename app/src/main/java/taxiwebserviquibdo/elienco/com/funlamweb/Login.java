7acc97a23f1f28269db175e960a0734fad3c5b32
jimmypelaez <jimmy.pelaez90@gmail.com>
2015-09-30 23:38:50 -0500

    mi primer commit

--- app/src/main/java/taxiwebserviquibdo/elienco/com/funlamweb/Login.java
+++ app/src/main/java/taxiwebserviquibdo/elienco/com/funlamweb/Login.java
@@ -1,16 +1,27 @@
 package taxiwebserviquibdo.elienco.com.funlamweb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends Activity {
+    EditText usuario= null;
+    EditText login= null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button login= (Button) findViewById(R.id.btnlogin);
+
+        login.setOnClickListener(btnlogin_Click);
+
     }
 
     @Override
@@ -20,6 +31,18 @@ public class Login extends Activity {
         return true;
     }
 
+    View.OnClickListener btnlogin_Click = new View.OnClickListener() {
+        @Override
+        public void onClick(View arg0) {
+             usuario= (EditText) findViewById(R.id.txtusuario);
+            login= (EditText) findViewById(R.id.txtlogin);
+          if (usuario.toString()=="prueba"&&login.toString()=="prueba"){
+              Intent intlogin = new Intent(Login.this, MainActivity.class);
+              startActivityForResult(intlogin, 0);
+              finish();
+          }
+        }
+    };
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
